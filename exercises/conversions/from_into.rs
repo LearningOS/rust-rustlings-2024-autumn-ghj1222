4611b4379aa31fd6cd0e6374b1ce3857274da9ba
ghj1222 <1342330485@qq.com>
2024-10-20 16:28:44 +0800

    do a lot except algo

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            let sp: Vec<&str> = s.split(',').collect();
+            if sp.len() != 2 {
+                Person::default()
+            } else {
+                let name = sp[0].to_string();
+                let age = sp[1].parse::<usize>();
+                if age.is_err() || name.len() == 0 {
+                    Person::default()
+                } else {
+                    Person { name, age: age.unwrap() }
+                }
+            }
+        }
     }
 }
 
